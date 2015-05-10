6e035bd49d554b879d586634a3437b8dbe1803ea
Yuto Manriki <yuto-manriki@cookpad.com>
2015-05-10 18:18:28 +0900

    人を登録可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def create
+    user = User.create(name: params[:name])
+    redirect_to user
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
