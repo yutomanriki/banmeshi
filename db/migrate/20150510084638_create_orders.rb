c70e3b17eab7ee676a4121979dd0542583ca8a21
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-05-10 17:47:43 +0900

    Create Order model

---
+++ db/migrate/20150510084638_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :name
+      t.string :comment
+
+      t.timestamps null: false
+    end
+  end
+end
